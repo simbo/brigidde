a4a4398a0f382cec23bea3e4b18a06ea20badfaa
Simon Lepel <simbo@simbo.de>
2018-02-24 16:27:22 +0100

    add route for favicon and reply with empty response

--- server/src/modules/server/routes.ts
+++ server/src/modules/server/routes.ts
@@ -1,5 +1,14 @@
 export const routes = [
 
+  // serve empty favicon
+  {
+    method: 'get',
+    path: '/favicon.ico',
+    handler(req, h) {
+      return h.response().code(200);
+    }
+  },
+
   // serve static files from /public
   {
     method: 'get',
