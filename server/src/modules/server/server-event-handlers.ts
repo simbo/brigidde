7afff465cb1180092882f99722485bde4de68489
Simon Lepel <simbo@simbo.de>
2018-02-24 17:46:26 +0100

    update server error handling to rewrite.to app index on html requests with 404

--- server/src/modules/server/server-event-handlers.ts
+++ server/src/modules/server/server-event-handlers.ts
@@ -1,16 +1,31 @@
+import { join } from 'path';
 import * as Accept from 'accept';
 
 export async function onPreResponse(req, h) {
 
-  // if response is error and preferred mediatype is html, respond with error view
-  if (
-    req.response.isBoom &&
-    Accept.parseAll(req.headers).mediaTypes[0] === 'text/html'
-  ) {
+  // if response is error
+  if (req.response.isBoom) {
     const error = req.response;
-    return h.view('error', { error })
-      .code(req.response.output.statusCode);
+
+    // if preferred mediatype is html
+    const reqHtml = Accept.parseAll(req.headers).mediaTypes[0] === 'text/html';
+    if (reqHtml) {
+
+      // if 404, rewrite to app index
+      if (error.output.statusCode === 404) {
+        return h.file(
+          join(process.env.APP_PUBLIC_PATH, 'index.html')
+        );
+      }
+
+      // respond with error view
+      return h.view('error', { error })
+        .code(error.output.statusCode);
+
+    }
+
   }
+
   return h.continue;
 
 }
