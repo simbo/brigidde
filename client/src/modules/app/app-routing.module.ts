4a058047f1e5cab83207148390c29a2d1f95bccf
Simon Lepel <simbo@simbo.de>
2018-02-24 17:44:57 +0100

    implement app routing and page-not-found component

---
+++ client/src/modules/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/terminal',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, {
+      enableTracing: false // only for debugging
+    })
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- client/src/modules/app/app.module.ts
+++ client/src/modules/app/app.module.ts
@@ -5,21 +5,25 @@ import { HttpModule } from '@angular/http';
 
 import { ServerModule } from './../server/server.module';
 import { TerminalModule } from './../terminal/terminal.module';
+import { AppRoutingModule } from './app-routing.module';
 import { MessageBusService } from './message-bus/message-bus.service';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpModule,
     ServerModule,
-    TerminalModule
+    TerminalModule,
+    AppRoutingModule
   ],
   providers: [
     MessageBusService
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   bootstrap: [
     AppComponent
