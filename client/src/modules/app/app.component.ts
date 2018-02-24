c9ff09fb2ff1c4a99846069c0f342df7dedd9aa0
Simon Lepel <simbo@simbo.de>
2018-02-24 16:30:00 +0100

    simplify/cleanup app component

--- client/src/modules/app/app.component.ts
+++ client/src/modules/app/app.component.ts
@@ -1,21 +1,11 @@
 import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { Observable } from 'rxjs/Observable';
-import { Subscription } from 'rxjs/Subscription';
-import { Subject } from 'rxjs/Subject';
-
-import { SocketService } from './../server/socket/socket.service';
 
 @Component({
   selector: 'body',
-  templateUrl: './app.component.pug',
-  styleUrls: ['./app.component.styl']
+  templateUrl: './app.component.pug'
 })
 export class AppComponent {
 
-  constructor(
-    private socketService: SocketService
-  ) {
-  }
+  constructor() {}
 
 }
