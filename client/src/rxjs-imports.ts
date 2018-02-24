ef6227c15d5cb6d06c49af40bedcc184f9016c2d
Simon Lepel <simbo@simbo.de>
2018-02-24 16:37:31 +0100

    add catch operator to rxjs imports

--- client/src/rxjs-imports.ts
+++ client/src/rxjs-imports.ts
@@ -5,7 +5,7 @@ import 'rxjs/BehaviorSubject';
 import 'rxjs/add/observable/of';
 // import 'rxjs/add/observable/fromPromise';
 // import 'rxjs/add/observable/throw';
-// import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/catch';
 // import 'rxjs/add/operator/combineLatest';
 import 'rxjs/add/operator/filter';
 // import 'rxjs/add/operator/finally';
