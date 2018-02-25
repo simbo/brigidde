ba98044c44ee0f860d7897996bb6f0aa61f47957
Simon Lepel <simbo@simbo.de>
2018-02-25 23:33:34 +0100

    refactor count command to be blocking by default and optionally non-blocking

--- client/src/modules/terminal/terminal-commands/lib/count/count.command.ts
+++ client/src/modules/terminal/terminal-commands/lib/count/count.command.ts
@@ -7,26 +7,39 @@ import { TerminalMessage } from '../../../terminal-message/terminal-message';
 export const countCommand: TerminalCommandHandler = {
 
   name: 'count',
-  usage: 'count <max> [<interval>]',
+  usage: 'count [--async] <max> [<interval>]',
   description: 'counts to given number',
 
   async run(parseArgs, output, unblock) {
-    const args = parseArgs() as ParsedArgs;
+    const args = parseArgs({
+      boolean: [
+        'async'
+      ],
+      alias: {
+        a: 'async'
+      }
+    }) as ParsedArgs;
     const maxValue = numberMinMax(parseInt(args._[0], 10) || 1, 0, 100);
+    const async = args.async ? true : false;
     const intervalDuration = numberMinMax(parseInt(args._[1], 10) || 500, 0, 60000);
     const { id } = new TerminalMessage('');
     let interval: number = null;
     let value: number = null;
-    const intervalFn = () => {
-      value = value === null ? 0 : (value + 1);
-      const body = `count: ${value}`;
-      const message = new TerminalMessage({ id, body });
-      output.next(message);
-      if (value >= maxValue) clearInterval(interval);
-    };
-    interval = window.setInterval(intervalFn, intervalDuration);
-    intervalFn();
-    unblock();
+    await new Promise((resolve, reject) => {
+      const intervalFn = () => {
+        value = value === null ? 0 : (value + 1);
+        const body = `count: ${value}`;
+        const message = new TerminalMessage({ id, body });
+        output.next(message);
+        if (value >= maxValue) {
+          clearInterval(interval);
+          resolve();
+        }
+      };
+      interval = window.setInterval(intervalFn, intervalDuration);
+      intervalFn();
+      if (async) unblock();
+    });
   }
 
 };
