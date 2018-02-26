2a613849b1aeb44a375dc6053995098707e31aee
Simon Lepel <simbo@simbo.de>
2018-02-26 11:16:03 +0100

    refactor count command to support per-line option which will use a new line for every output or otherwise update the existing message

--- client/src/modules/terminal/terminal-commands/lib/count/count.command.ts
+++ client/src/modules/terminal/terminal-commands/lib/count/count.command.ts
@@ -1,35 +1,40 @@
 import { ParsedArgs } from 'minimist';
 
 import { numberMinMax } from './../../../../generic/number-min-max';
+import { TerminalMessage } from './../../../terminal-message/terminal-message';
+import { TerminalMessageData } from './../../../terminal-message/terminal-message-data.interface';
 import { TerminalCommandHandler } from './../../terminal-command-handler.interface';
-import { TerminalMessage } from '../../../terminal-message/terminal-message';
 
 export const countCommand: TerminalCommandHandler = {
 
   name: 'count',
-  usage: 'count [--async] <max> [<interval>]',
+  usage: 'count [--async] [--per-line] <max> [<interval>]',
   description: 'counts to given number',
 
   async run(parseArgs, output, unblock) {
     const args = parseArgs({
       boolean: [
-        'async'
+        'async',
+        'per-line'
       ],
       alias: {
-        a: 'async'
+        a: 'async',
+        p: 'per-line',
+        l: 'per-line'
       }
     }) as ParsedArgs;
-    const maxValue = numberMinMax(parseInt(args._[0], 10) || 1, 0, 100);
     const async = args.async ? true : false;
+    const perLine = args['per-line'] ? true : false;
+    const maxValue = numberMinMax(parseInt(args._[0], 10) || 1, 0, 100);
     const intervalDuration = numberMinMax(parseInt(args._[1], 10) || 500, 0, 60000);
-    const { id } = new TerminalMessage('');
+    const baseMessage = new TerminalMessage('');
     let interval: number = null;
     let value: number = null;
     await new Promise((resolve, reject) => {
       const intervalFn = () => {
         value = value === null ? 0 : (value + 1);
-        const body = `count: ${value}`;
-        const message = new TerminalMessage({ id, body });
+        const message = perLine ? new TerminalMessage('') : baseMessage;
+        message.setBody(`count: ${value}`);
         output.next(message);
         if (value >= maxValue) {
           clearInterval(interval);
