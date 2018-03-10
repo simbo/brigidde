375d93e4e2a612b1f6469dc20480882dc81c3841
Simon Lepel <simbo@simbo.de>
2018-03-10 01:28:55 +0100

    add sort-on as dependency to replace sort-objects-by-property

--- server/src/modules/chatbot/request-resolvers/wit/wit-resolver.ts
+++ server/src/modules/chatbot/request-resolvers/wit/wit-resolver.ts
@@ -1,6 +1,6 @@
 import { Wit, MessageResponse } from 'node-wit';
+import * as sortOn from 'sort-on';
 
-import { sortObjectsByProperty } from './../../../shared/sort-object-by-property';
 import { Intent } from './../../intent-resolvers/intent.interface';
 import { RequestResolverFunction } from './../request-resolver-function.interface';
 import { RequestResolverResult } from './../request-resolver-result.interface';
@@ -26,8 +26,8 @@ function getMostConfidentIntent(msg: MessageResponse): Intent {
     // filter and sort descending by confidence
     .filter((type) => {
       entities[type] = (msg.entities[type] as any[])
-        .filter((entity) => entity.confidence >= minConfidence)
-        .sort(sortObjectsByProperty('confidence', 'desc'));
+        .filter((entity) => entity.confidence >= minConfidence);
+      entities[type] = sortOn(entities[type], '-confidence');
       return entities[type].length > 0;
     })
     // reduce to most confident intent from all entities
